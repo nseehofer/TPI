cf64b0058663356afbac3e80fcac18b26ea1167f
nseehofer <nicolasseehofer.99@hotmail.com>
2024-11-06 23:06:07 -0300

    ULTIMO COMMIT PARA CURSOS DINAMICOS
    Trabaje dentro del JS "js_modal_detalle.js" "index.html" y "detalleDeCurso.html"
    
    - Se crea JSON con cursos para tomar sus datos y "pintar" cada detalle y card de los respectivos cursos y utilizar solo UNA template como modelo de tal.
    
    - Se agrega el SimuladorDeDatos o MockData al localStorage
    
    - Se borran todas las paginas detalleCurso y queda una sola como modelo para ser "pintada", y se utilizan clases TODAS CON EL PREFIJO "js-..." para ser utilizadas en el codigo JavaScript
    
    - Se agrega un ID en cada <a> donde se puede "Ver Detalle" de cada curso desde el "index.html"

--- js/js_modal_detalle.js
+++ js/js_modal_detalle.js
@@ -1,9 +1,78 @@
-const cursos = [
-    { id: 1, nombre: 'Diseño UX/UI', precio: 20000, duracion: '13 meses', modalidad: 'Virtual', imagen: '../images/uxui.png' },
-    { id: 2, nombre: 'Ciberseguridad', precio: 50000, duracion: '5 meses', modalidad: 'Virtual', imagen: '../images/ciberseguridad.png' },
-    { id: 3, nombre: 'Desarrollo Full Stack', precio: 60000, duracion: '6 meses', modalidad: 'Virtual', imagen: '../images/fullstack.jpg' },
-    { id: 4, nombre: 'Desarrollo Front End', precio: 45000, duracion: '4 meses', modalidad: 'Virtual', imagen: '../images/front.jpg' }
-];
+const datosSimulados = {
+    cursos: [{
+        id: 1,
+        identificadorCurso : 'curso_uxui',
+        nombre: 'Diseño UX/UI', // js-titulo-curso
+        precio: 20000, // js-precio-curso
+        duracion: '13 meses', // js-modalidad-curso
+        modalidad: 'Virtual', // js-duracion-curso
+        imagen: '../images/uxui.png',
+        requisitosPrevios: 'Conocimiento básico de Diseño', // js-requisitos-curso // js-descripcion-curso
+        descripcion: 'Aprende los principios fundamentales del diseño UX/UI para crear experiencias de usuario atractivas y funcionales en plataformas digitales', // js-descripcion-curso
+        modulo1: 'Definiciones básicas de UX y UI', //js-modulo1
+        modulo2: 'Técnicas de investigación de usuarios', //js-modulo2
+        modulo3: 'Arquitectura de la información', //js-modulo3
+        modulo4: 'Prototipado con herramientas como Figma y Adobe XD', //js-modulo4
+        imagenInstructor: '../images/profesor.png',
+        instructor: 'Juan Garay' //js-instructor
+    },
+    {
+        id: 2,
+        identificadorCurso : 'curso_ciber', 
+        nombre: 'Ciberseguridad',
+        precio: 50000,
+        duracion: '5 meses',
+        modalidad: 'Virtual',
+        imagen: '../images/ciberseguridad.png',
+        requisitosPrevios: 'Conocimiento básico de redes',
+        descripcion: ' Aprende los conceptos básicos de ciberseguridad para proteger redes y sistemas contra amenazas y ataques.',
+        modulo1: 'Introducción a la ciberseguridad.',
+        modulo2: 'Amenazas y vulnerabilidades en redes.',
+        modulo3: 'Seguridad en aplicaciones web.',
+        modulo4: 'Protocolos y normativas de seguridad.',
+        imagenInstructor: '../images/profesor.png',
+        instructor: 'Nazareno Montoya'
+    },
+    {
+        id: 3,
+        identificadorCurso : 'curso_full',
+        nombre: 'Desarrollo Full Stack',
+        precio: 60000,
+        duracion: '6 meses',
+        modalidad: 'Virtual',
+        imagen: '../images/fullstack.jpg',
+        requisitosPrevios: 'Conocimientos en desarrollo web básico (HTML, CSS, JavaScript)',
+        descripcion: 'Aprende a desarrollar aplicaciones completas, tanto en el frontend como en el backend, utilizando tecnologías modernas.',
+        modulo1: 'Fundamentos de desarrollo frontend.',
+        modulo2: 'Desarrollo backend con Node.js.',
+        modulo3: 'Integración frontend y backend.',
+        modulo4: 'Despliegue de aplicaciones web.',
+        imagenInstructor: '../images/profesor.png',
+        instructor: 'Jorge Narvaez'
+    },
+    {
+        id: 4,
+        identificadorCurso : 'curso_front',
+        nombre: 'Desarrollo Front End',
+        precio: 45000, duracion: '4 meses',
+        modalidad: 'Virtual',
+        imagen: '../images/front.jpg',
+        requisitosPrevios: 'Conocimientos básicos de HTML y CSS',
+        descripcion: ' Aprende a desarrollar interfaces web modernas y atractivas utilizando HTML, CSS y JavaScript.',
+        modulo1: 'Fundamentos de HTML y CSS.',
+        modulo2: 'Diseño responsivo con CSS.',
+        modulo3: 'Introducción a JavaScript.',
+        modulo4: 'Desarrollo de un proyecto web completo.',
+        imagenInstructor: '../images/profesor.png',
+        instructor: 'Mariano Rodriguez'
+    }
+    ]
+    // REVISAR QUE OTROS ELEMENTOS FALTAN AGREGAR AL LOCAL, POR AHORA SOLO ESTAN LOS CURSOS CON SUS DATOS ESCENCIALES
+};
+
+localStorage.setItem("datos",JSON.stringify(datosSimulados));
+
+// AGREGUE EL 'MOCKDATA == DATOSSIMULADOS' Y LOS PUSE EN EL LOCALSTORAGE PARA GENERAR LOS CURSOS DINAMICOS
 
 const modal = document.getElementById("modal-inscripcion");
 const inscribirseBtn = document.querySelector(".btn.btn-azul");
@@ -16,7 +85,7 @@ const modalMode = document.getElementById("modal-course-mode");
 const modalDuration = document.getElementById("modal-course-duration");
 const modalPrice = document.getElementById("modal-course-price");
 
-inscribirseBtn.addEventListener("click", function(event) {
+inscribirseBtn.addEventListener("click", function (event) {
     event.preventDefault();
 
     const courseTitle = document.querySelector(".course-title").textContent;
@@ -64,12 +133,12 @@ function agregarCursoAlCarrito() {
 function cerrarModal() {
     modal.style.display = "none";
     agregarCursoAlCarrito();
-    mostrarNumeroYContenidoCarrito(); 
+    mostrarNumeroYContenidoCarrito();
 }
 
 closeBtn.addEventListener("click", cerrarModal);
 closeModalBtn.addEventListener("click", cerrarModal);
-window.addEventListener("click", function(event) {
+window.addEventListener("click", function (event) {
     if (event.target == modal) {
         cerrarModal();
     }
@@ -143,7 +212,7 @@ function trasladarCarritoAlHeader() {
     const removerCarritoDeLosBotones = document.querySelector('#remover-carrito-de-media-query');
     if (removerCarritoDeLosBotones) {
         removerCarritoDeLosBotones.remove();
-    } 
+    }
     const contenedorLogo = document.querySelector('.js-logo-container');
     let crearDivParaContenedorCarrito = document.querySelector('.js_contenedor_carrito');
     if (!crearDivParaContenedorCarrito) {
@@ -159,15 +228,15 @@ function trasladarCarritoAlHeader() {
             <ul class="carrito-lista"></ul>`;
         contenedorLogo.appendChild(crearDivParaContenedorCarrito);
         const contenedorCarritoLista = document.querySelector('.carrito-lista');
-        contenedorCarritoLista.style.left ='-420%';
+        contenedorCarritoLista.style.left = '-420%';
     }
 }
 
 function verificarMediaQuery() {
-    
+
     const mediaQuery768 = window.matchMedia('(max-width: 768px)');
     const mediaQuery575 = window.matchMedia('(max-width: 575px)');
-    if ( mediaQuery768.matches || mediaQuery575.matches) {
+    if (mediaQuery768.matches || mediaQuery575.matches) {
         trasladarCarritoAlHeader();
     }
 }
